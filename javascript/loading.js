78a79de5b4d2d6ecafd07fc5bc9ef0c51323a887
Hayden Worden <haydenworden@outlook.com>
2018-04-23 18:37:44 +0100

    Loading page

--- javascript/loading.js
+++ javascript/loading.js
@@ -1,4 +1,7 @@
 function loadingFunc(){
     var loading = document.querySelector("section");
+    var body = document.querySelector("body");
+    body.classList.add("loading")
     loading.classList.remove("page-load")
+    body.classList.remove("loading")
 }
