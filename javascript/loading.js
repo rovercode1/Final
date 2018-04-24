2b971c0bcace1026ff39e66fa90bce70794c051d
Hayden Worden <haydenworden@outlook.com>
2018-04-24 08:47:53 +0100

    Animations changed

--- javascript/loading.js
+++ javascript/loading.js
@@ -1,8 +1,14 @@
 function loadingFunc(){
-    var loading = document.querySelector("section");
-    var body = document.querySelector("body");
-    body.classList.add("loading")
-    loading.classList.remove("page-load")
-    loading.classList.add("loaded")
-    body.classList.remove("loading")
+  headerLoad()
+  var loading = document.querySelector("section");
+  var body = document.querySelector("body");
+  body.classList.add("loading")
+  loading.classList.remove("page-load")
+  loading.classList.add("loaded")
+  body.classList.remove("loading")
+}
+var section = document.getElementById("first");
+
+function headerLoad(){
+  section.classList.add("show");
 }
